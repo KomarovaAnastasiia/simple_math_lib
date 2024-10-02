2780551053f221dfd873bddab1a72b6b9c589945
KomarovaAnastasiia <117724892+KomarovaAnastasiia@users.noreply.github.com>
2024-10-02 13:24:06 +0300

    Update math_lib.h

--- src/math_lib/include/math_lib.h
+++ src/math_lib/include/math_lib.h
@@ -1,38 +1,19 @@
-#pragma once
+#include "math_lib.h"
 
-namespace MathLib
-{
-	// Function for addition
-	template <typename T>
-	T add(T a, T b)
-	{
-		return a + b;
-	}
-	
-	// Function for subtraction
-	template <typename T>
-	T subtract(T a, T b)
-	{
-		return a - b;
-	}
-	
-	// Function for multiplication
-	template <typename T>
-	T multiply(T a, T b)
-	{
-		return a * b;
-	}
-	
-	// Function for division
-	template <typename T>
-	T divide(T a, T b)
-	{
-		if (b == 0)
-		{
-			throw std::invalid_argument("Division by zero is not allowed.");
-		}
-		return a / b;
-	}
-	
-	bool isEqual(double a, double b, double tolerance = 0.000000001);
+// Функція для обчислення квадратного кореня методом Ньютона
+double sqrt_newton(double number, double epsilon) {
+    if (number < 0) {
+        return -1; // Помилка, оскільки неможливо знайти корінь від від'ємного числа
+    }
+
+    double guess = number; // Початкове припущення
+    double result = 0.0;
+
+    // Метод Ньютона: поки різниця між поточним припущенням і результатом більше, ніж epsilon
+    while ((guess - result) > epsilon) {
+        result = guess;
+        guess = (guess + number / guess) / 2; // Формула методу Ньютона
+    }
+
+    return guess;
 }
